d701a5db119da614ee5815c4bbb3795f3f48ce9b
mhdpronos <mo64166946@gmail.com>
2025-10-21 12:59:42 +0000

    Update index.js

--- fonctions/index.js
+++ fonctions/index.js
@@ -1,1 +1,33 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
 
+admin.initializeApp();
+
+// 🔐 Configure ton adresse Gmail ici
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "mo64166946@gmail.com", // remplace par ton email Gmail
+    pass: "catvoboxzrspuzuc"
+  }
+});
+
+// ✅ Fonction pour envoyer un email personnalisé
+exports.envoyerEmail = functions.https.onCall(async (data, context) => {
+  const { email, sujet, message } = data;
+
+  const mailOptions = {
+    from: "MHD Pronos <mo64166946@gmail.com>",
+    to: email,
+    subject: sujet,
+    html: `<p>${message}</p>`
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return { success: true, message: "E-mail envoyé avec succès 🚀" };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+});
